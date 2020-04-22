f1f8764474dcc5e2704a4d1ffded868dc194c416
tsumo <tsumo@users.noreply.github.com>
2020-04-22 19:50:42 +0300

    First version of renderer
    My main objective with createRenderer was to preserve type information.
    Resulting renderer function remembers types from the passed class list.
    Totally type-safe!
    
    One surprise was that in typescript Object.keys looses type information
    on purpose, due to dynamic nature of js in the wild. Quick googling
    led to the easy solution - write your own Object.keys.
    
    I'm really happy that I could get through the TS generics and utility
    types and arrive at the working solution!

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,11 +1,15 @@
-import richTextToHTML from '../index'
+import { createRenderer } from '../index'
 
-test('Testing richTextToHTML', () => {
-  expect(richTextToHTML({ text: 'testing', attr: ['bold'] })).toBe(
-    '<b>testing</b>',
+test('Testing createRenderer', () => {
+  const renderer = createRenderer({ bold: 'boldClass', italic: 'italicClass' })
+
+  expect(renderer([{ bold: 'some-bold-text' }])).toBe(
+    '<p class="boldClass">some-bold-text</p>',
   )
 
-  expect(richTextToHTML({ text: 'testing', attr: ['italic', 'bold'] })).toBe(
-    '<b><i>testing</i></b>',
+  expect(
+    renderer([{ italic: 'some-italic-text' }, { bold: 'some-bold-text' }]),
+  ).toBe(
+    '<p class="italicClass">some-italic-text</p><p class="boldClass">some-bold-text</p>',
   )
 })
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-type RichText = {
-  text: string
-  attr: ('bold' | 'italic')[]
+// From https://github.com/Microsoft/TypeScript/pull/12253#issuecomment-353494273
+// Fixes Object.keys loosing type info
+const keys = <O>(o: O) => {
+  return Object.keys(o) as (keyof O)[]
 }
 
-const richTextToHTML = (rt: RichText) => {
-  let left = ''
-  let right = ''
-  if (rt.attr.includes('bold')) {
-    left += '<b>'
-    right = '</b>' + right
+export const createRenderer = <T extends string>(
+  classes: Record<T, string>,
+): ((node: Partial<Record<T, string>>[]) => string) => {
+  return (texts) => {
+    let result = ''
+    texts.forEach((item) => {
+      keys(item).forEach((key) => {
+        result += `<p class="${classes[key]}">${item[key]}</p>`
+      })
+    })
+    return result
   }
-  if (rt.attr.includes('italic')) {
-    left += '<i>'
-    right = '</i>' + right
-  }
-  return left + rt.text + right
 }
-
-export default richTextToHTML
