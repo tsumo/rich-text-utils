9ad8b82d325dca54b0a574d6d89a8cd00c8b4a8e
tsumo <tsumo@users.noreply.github.com>
2020-04-23 19:10:25 +0300

    Allow to specify tag name in format description

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,15 +1,28 @@
 import { createRenderer } from '../index'
 
-test('Testing createRenderer', () => {
-  const renderer = createRenderer({ bold: 'boldClass', italic: 'italicClass' })
+test('createRenderer basic functionality', () => {
+  const renderer = createRenderer({
+    bold: { className: 'bold-class', tag: 'div' },
+    italic: { className: 'italic-class' },
+  })
 
-  expect(renderer([{ bold: 'some-bold-text' }])).toBe(
-    '<p class="boldClass">some-bold-text</p>',
+  expect(renderer([{ bold: 'some bold text' }])).toBe(
+    '<div class="bold-class">some bold text</div>',
   )
 
   expect(
-    renderer([{ italic: 'some-italic-text' }, { bold: 'some-bold-text' }]),
+    renderer([{ italic: 'some italic text' }, { bold: 'some bold text' }]),
   ).toBe(
-    '<p class="italicClass">some-italic-text</p><p class="boldClass">some-bold-text</p>',
+    '<span class="italic-class">some italic text</span><div class="bold-class">some bold text</div>',
+  )
+})
+
+test('empty string in tag defaults to span', () => {
+  const renderer = createRenderer({
+    bold: { className: 'bold-class', tag: '' },
+  })
+
+  expect(renderer([{ bold: 'some bold text' }])).toBe(
+    '<span class="bold-class">some bold text</span>',
   )
 })
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ const keys = <O>(o: O) => {
   return Object.keys(o) as (keyof O)[]
 }
 
+type HtmlFormat = {
+  className: string
+  tag?: string
+}
+
 export const createRenderer = <T extends string>(
-  classes: Record<T, string>,
+  classes: Record<T, HtmlFormat>,
 ): ((node: Partial<Record<T, string>>[]) => string) => {
   return (texts) => {
     let result = ''
     texts.forEach((item) => {
       keys(item).forEach((key) => {
-        result += `<p class="${classes[key]}">${item[key]}</p>`
+        const tag = classes[key].tag || 'span'
+        result += `<${tag} class="${classes[key].className}">${item[key]}</${tag}>`
       })
     })
     return result
