c7e5d133c51130ca3cbeabb66320c0c3a960adeb
tsumo <tsumo@users.noreply.github.com>
2020-04-25 16:31:39 +0300

    Custom renderer with formatter callbacks
    I've changed data shape from array of objects
      [ {type1: text}, {type2: text} ]
    to array of tuples
      [ [type1, text], [type2, text] ]
    
    Everything is still strictly type-checked.

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,28 +1,51 @@
-import { createRenderer } from '../index'
+import { createHtmlRenderer, createCustomRenderer } from '../index'
 
-test('createRenderer basic functionality', () => {
-  const renderer = createRenderer({
-    bold: { className: 'bold-class', tag: 'div' },
-    italic: { className: 'italic-class' },
+describe('createHtmlRenderer', () => {
+  test('basic functionality', () => {
+    const renderer = createHtmlRenderer({
+      bold: { className: 'bold-class', tag: 'div' },
+      italic: { className: 'italic-class' },
+    })
+
+    expect(renderer([['bold', 'some bold text']])).toBe(
+      '<div class="bold-class">some bold text</div>',
+    )
+
+    expect(
+      renderer([
+        ['italic', 'some italic text'],
+        ['bold', 'some bold text'],
+      ]),
+    ).toBe(
+      '<span class="italic-class">some italic text</span><div class="bold-class">some bold text</div>',
+    )
   })
 
-  expect(renderer([{ bold: 'some bold text' }])).toBe(
-    '<div class="bold-class">some bold text</div>',
-  )
+  test('empty string in tag defaults to span', () => {
+    const renderer = createHtmlRenderer({
+      bold: { className: 'bold-class', tag: '' },
+    })
 
-  expect(
-    renderer([{ italic: 'some italic text' }, { bold: 'some bold text' }]),
-  ).toBe(
-    '<span class="italic-class">some italic text</span><div class="bold-class">some bold text</div>',
-  )
+    expect(renderer([['bold', 'some bold text']])).toBe(
+      '<span class="bold-class">some bold text</span>',
+    )
+  })
 })
 
-test('empty string in tag defaults to span', () => {
-  const renderer = createRenderer({
-    bold: { className: 'bold-class', tag: '' },
-  })
+describe('createCustomRenderer', () => {
+  test('basic functionality', () => {
+    const renderer = createCustomRenderer({
+      bold: (text) => `## ${text} ##`,
+      italic: (text) => `__ ${text} __`,
+    })
 
-  expect(renderer([{ bold: 'some bold text' }])).toBe(
-    '<span class="bold-class">some bold text</span>',
-  )
+    expect(renderer([['bold', 'some bold text']])).toBe('## some bold text ##')
+
+    expect(
+      renderer([
+        ['italic', 'some italic text'],
+        ['bold', 'some bold text'],
+      ]),
+    ).toBe('__ some italic text __## some bold text ##')
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-// From https://github.com/Microsoft/TypeScript/pull/12253#issuecomment-353494273
-// Fixes Object.keys loosing type info
-const keys = <O>(o: O) => {
-  return Object.keys(o) as (keyof O)[]
-}
-
 type HtmlFormat = {
   className: string
   tag?: string
 }
 
-export const createRenderer = <T extends string>(
+export const createHtmlRenderer = <T extends string>(
   classes: Record<T, HtmlFormat>,
-): ((node: Partial<Record<T, string>>[]) => string) => {
+): ((data: [T, string][]) => string) => {
   return (texts) => {
     let result = ''
-    texts.forEach((item) => {
-      keys(item).forEach((key) => {
-        const tag = classes[key].tag || 'span'
-        result += `<${tag} class="${classes[key].className}">${item[key]}</${tag}>`
-      })
+    texts.forEach(([format, text]) => {
+      const tag = classes[format].tag || 'span'
+      result += `<${tag} class="${classes[format].className}">${text}</${tag}>`
+    })
+    return result
+  }
+}
+
+type CustomFormatter = (text: string) => string
+
+export const createCustomRenderer = <T extends string>(
+  formatters: Record<T, CustomFormatter>,
+): ((data: [T, string][]) => string) => {
+  return (texts) => {
+    let result = ''
+    texts.forEach(([format, text]) => {
+      result += formatters[format](text)
     })
     return result
   }
